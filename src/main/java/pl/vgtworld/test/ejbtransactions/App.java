5e957f0ba2a17f4586bc8164a3d1259d3090d832
VGT <tomek@vgtworld.pl>
2015-08-30 11:58:20 +0200

    Add application configuration.

---
+++ src/main/java/pl/vgtworld/test/ejbtransactions/App.java
@@ -0,0 +1,8 @@
+package pl.vgtworld.test.ejbtransactions;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/tests")
+public class App extends Application {
+}
